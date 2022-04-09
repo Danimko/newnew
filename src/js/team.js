413d67c175f1201f82d7e08b83778b79731993fa
Danimko <97961582+Danimko@users.noreply.github.com>
2022-04-09 08:40:24 +0300

    Update team.js

--- src/js/team.js
+++ src/js/team.js
@@ -1,7 +1,4 @@
 ;(function() {
-// const teamList = document.querySelector(".team__list");
-// const buttonTeam = document.querySelectorAll('.team__button');
-// const svgTeam = document.querySelectorAll('.team__svg');
 const openItem = item => {
   const container = item.closest(".team__item");
   const contentWrapper = container.find(".team__desc-wrapper");
@@ -41,67 +38,3 @@ $(".team__button").click(e => {
 })()
 
 
-// buttonTeam.addEventListener("click", function (e) {
-//     const this = e.currentTarget;
-//     openItem(this);
-//     .classList.add("team__desc--active");
-// })
-
-
-// $(buttonTeam).click(e=>{
-//   const $this = $(e.currentTarget);
-
-//   openItem($this);
-// })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// for (let index = 0; index < buttonTeam.length; index++) {
-//   const button = buttonTeam[index];
-  
-//   button.addEventListener("click", function (e) {
-//     const target = e.target;
-//     const desc = target.;
-//     console.log("piy", desc);
-
-    
-     // const target = e.target;
-    // const desc = target.nextElementSibling;
-    // console.log("dese", desc);
-    // desc.classList.add("team__desc--active");
-//   });
-// }
-
-
-
-// linkModalMenu.forEach(item => {
-//   item.addEventListener("click", function(e) {
-//     linkModalMenu.forEach(el=>{el.classList.remove("mmlinkactive");});
-//   item.classList.add("mmlinkactive")
-//   if (item.classList.contains("mmlinkactive")) {
-//     modalMenu.classList.remove("modalmenu-on");
-//   } 
-// })
-// })
-
-
-
-
-// for(i = 0; i < buttonTeam.length; i++){
-//   buttonTeam[i].addEventListener('click', function(event){
-//     event.preventDefault();
